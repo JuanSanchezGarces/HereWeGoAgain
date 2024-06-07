231d0ebfe89f8dc8c306585639a3a7ae8ed9bac1
JuanSanchezGarces <juanfsanchezd@gmail.com>
2024-06-06 21:14:39 -0300

    Notificacao adicionada pros lembretes

--- app/src/main/java/com/example/idosos/AddReminderActivity.java
+++ app/src/main/java/com/example/idosos/AddReminderActivity.java
@@ -1,22 +1,37 @@
 package com.example.idosos;
 
+import android.Manifest;
 import android.app.DatePickerDialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.TimePickerDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
+import android.os.Handler;
+import android.os.Looper;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -34,10 +49,14 @@ public class AddReminderActivity extends AppCompatActivity implements ReminderAd
     private SharedPreferences sharedPreferences;
     private Gson gson;
 
+    private static final int NOTIFICATION_PERMISSION_CODE = 123;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.reminder_layout);
+
+        createNotificationChannel();
 
         sharedPreferences = getSharedPreferences("Reminders", Context.MODE_PRIVATE);
         gson = new Gson();
@@ -55,28 +74,13 @@ public class AddReminderActivity extends AppCompatActivity implements ReminderAd
         remindersRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         remindersRecyclerView.setAdapter(reminderAdapter);
 
-        addReminderButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                addReminder();
-            }
-        });
+        addReminderButton.setOnClickListener(v -> addReminder());
 
         ImageView calendarIcon = findViewById(R.id.calendarIcon);
-        calendarIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openDatePicker();
-            }
-        });
+        calendarIcon.setOnClickListener(v -> openDatePicker());
 
         ImageView timeIcon = findViewById(R.id.timeIcon);
-        timeIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openTimePicker();
-            }
-        });
+        timeIcon.setOnClickListener(v -> openTimePicker());
     }
 
     private void openDatePicker() {
@@ -128,7 +132,12 @@ public class AddReminderActivity extends AppCompatActivity implements ReminderAd
         reminderAdapter.notifyDataSetChanged();
         saveReminders();
 
-        // Código para definir o lembrete com AlarmManager se necessário
+        // Calculate delay
+        long currentTimeMillis = System.currentTimeMillis();
+        long delay = calendar.getTimeInMillis() - currentTimeMillis;
+
+        // Schedule notification
+        scheduleNotification(title, "É hora do seu lembrete!", delay);
 
         Toast.makeText(this, "Lembrete adicionado", Toast.LENGTH_SHORT).show();
     }
@@ -150,5 +159,85 @@ public class AddReminderActivity extends AppCompatActivity implements ReminderAd
     @Override
     public void onReminderRemoved() {
         saveReminders();
+    }
+
+    private void scheduleNotification(String title, String message, long delay) {
+        Handler handler = new Handler(Looper.getMainLooper());
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                if (hasPermissions() && isNotificationChannelEnabled()) {
+                    sendNotification(title, message);
+                }
+            }
+        };
+        handler.postDelayed(runnable, delay);
+    }
+
+    private boolean hasPermissions() {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.VIBRATE) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.WAKE_LOCK) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private boolean isNotificationChannelEnabled() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            if (notificationManager != null) {
+                NotificationChannel channel = notificationManager.getNotificationChannel("reminder_channel");
+                return channel != null && channel.getImportance() != NotificationManager.IMPORTANCE_NONE;
+            }
+        }
+        return true; // Se não estiver no Android Oreo ou posterior, supõe-se que as notificações estão habilitadas
+    }
+
+    private void sendNotification(String title, String message) {
+        try {
+            // Tentar enviar a notificação
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "reminder_channel")
+                    .setSmallIcon(R.drawable.ic_notification2)
+                    .setContentTitle(title)
+                    .setContentText(message)
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setAutoCancel(true);
+
+            Intent intent = new Intent(this, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+            builder.setContentIntent(pendingIntent);
+
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+            notificationManager.notify(1, builder.build());
+        } catch (SecurityException e) {
+            // Lidar com a exceção de segurança
+            e.printStackTrace();
+            // Por exemplo, mostrar uma mensagem de erro ao usuário
+            Toast.makeText(this, "Erro ao enviar notificação: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Reminder Channel";
+            String description = "Channel for Reminder Notifications";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("reminder_channel", name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == NOTIFICATION_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permissão concedida
+                Toast.makeText(this, "Permissão concedida. Notificação será enviada.", Toast.LENGTH_SHORT).show();
+            } else {
+                // Permissão negada
+                Toast.makeText(this, "Permissão negada. Não será possível enviar a notificação.", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/idosos/ReminderReceiver.java
+++ app/src/main/java/com/example/idosos/ReminderReceiver.java
@@ -1,19 +1,32 @@
 package com.example.idosos;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
+import androidx.core.app.NotificationCompat;
 
 public class ReminderReceiver extends BroadcastReceiver {
+    private static final String CHANNEL_ID = "reminder_channel";
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // Extrai a mensagem do lembrete do intent
-        String reminderMessage = intent.getStringExtra("reminder_message");
+        String title = intent.getStringExtra("title");
+        String message = intent.getStringExtra("message");
 
-        // Exibe a mensagem do lembrete usando um Toast
-        Toast.makeText(context, reminderMessage, Toast.LENGTH_SHORT).show();
+        Intent notificationIntent = new Intent(context, AddReminderActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_notification) // Altere este ícone conforme necessário
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify((int) System.currentTimeMillis(), builder.build());
     }
 }
-
